5a0371e6d6ea7b9b11806aa11c77c6adbce60a62
Gradoos-code <64257619+Gradoos-code@users.noreply.github.com>
2022-12-24 13:34:31 +0300

    Create lessonFour.cs

---
+++ lessonFour.cs
@@ -0,0 +1,59 @@
+// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// Два варианта решения:
+// программа:
+
+// int num1 = Convert.ToInt32(console.Readline());
+// int num2 = Convert.ToInt32(console.Readline());
+
+// double cube = Math.Pow(num1, num2);
+
+// console.Writeline(cube);
+
+// // цикл:
+
+
+// int num1 = Convert.ToInt32(console.Readline());
+// int num2 = Convert.ToInt32(console.Readline());
+// int cube=1;
+// for (i=1;i<=num2;i++ )
+// {
+//   int cube *= num1;
+// }
+// console.Writeline(cube);
+
+
+// // Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// console.Writeline("Введите число");
+
+// int num = Convert.ToInt32(console.Readline());
+// int sum=0;
+
+// int func()
+// {
+//     while(num>0)
+//     {
+//         int temp = num%10;
+//         sum +=temp;
+//         num/=10;
+//     }
+// return sum;
+// }
+// Console.Writeline("Сумма цифр числа равна "+func());
+
+// // Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+// int [] massive();
+// {
+//     int [] array= new int[8];
+//     for(i=0; i<array.lenght; i++)
+//     {
+//         Console.Writeline("Введите элемент массива \n");
+//         array[i] = convert.ToInt32(Console.Readline());
+
+//     }
+//     array return
+
+// }
+
+// Console.Writeline(string.Join(",",massive()));
