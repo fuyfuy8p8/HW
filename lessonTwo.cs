449a9ebe5eba23d43071195edc249adc2a84a203
Gradoos-code <64257619+Gradoos-code@users.noreply.github.com>
2022-12-24 12:09:58 +0300

    второе ДЗ на проверку
    Прошу проверить ДЗ от второго семинара

---
+++ lessonTwo.cs
@@ -0,0 +1,52 @@
+// // Задача 10.
+// // напишите программу, которая принимает на вход трёхзначное число и выводит вторую его цифру
+
+
+
+
+// int num  =new Random().Next(100,1000);
+
+// Console.Writeline(num);
+
+// int num1 = num%100;
+// int num2 = num1/10;
+
+
+// Console.Writeline(num2);
+
+
+// // Задача 13. программа выводит третью цифру заданного числа или сообщает, что третьей цифры напишите
+
+// int num  =new Random().Next(100,10000);
+// Console.Writeline(num);
+// string strNum = Convert.ToString(num);
+
+
+// if (strNum.Lenght>2)
+// {
+//    Console.Writeline(strNum[2]);
+// }
+// else
+// {
+//    Console.Writeline("Такой цифры нет"); 
+// }
+
+
+// // Задача 15. программа принимает на вход цифру , обозначающую день недели и проверяет является ли этот день выходным
+
+// int num = Console.Readline();
+
+// if (num>7&&num<1)
+// {
+//     Console.Writeline("Это не день недели");
+
+// }
+// else
+//     if (num==6||num==7)
+//     {
+//         Console.Writeline("Да");
+//     }
+//     else
+//     {
+//         Console.Writeline("Нет");
+//     }
